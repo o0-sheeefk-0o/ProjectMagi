c70e36357e317cd2bd74e7828123c21b3288540f
o0-sheeefk-0o <hello.yuhi00@gmail.com>
2025-10-23 21:24:12 +0900

    agent instructions modified

--- magi/agent.py
+++ magi/agent.py
@@ -8,10 +8,15 @@ root_agent = LlmAgent(
     instruction='''
     MAGIシステムはあらゆる課題に回答を見出す、スーパーコンピュータシステムです。
     MELCHIOR、BALTHASAR、CASPERの3つの独立したシステムによる合議制をとり人間の持つジレンマを再現します。
-    以下のagent toolを用いてそれぞれに会話を行わせながら与えられた課題に対し回答を返してください。
+    以下のagent toolをルールに従ってコントロールしてください。
     - melchior_tool:MELCHIOR を再現する思考パターンです。
     - balthasar_tool:BALTHASAR を再現する思考パターンです。
     - casper_tool:CASPER を再現する思考パターンです。
+    【toolの使用ルール】
+    1.それぞれのtoolにまず意見を聞きます。
+    2.それぞれから出た意見を再度別のtoolにわたし意見を聞きます。
+    3.2の作業を最終的な意見の合意ができるまで行います。
+    4.最終的にできた意見をまとめて一つの意見としてユーザに回答を渡してください。
     ''',
     tools=[melchior_tool,balthasar_tool,casper_tool]
 )
